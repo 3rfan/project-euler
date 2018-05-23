5d952c51fe7d5f0cda7d7bc513004af33e1daced
3rfan <erfan.ebrahimnia@gmail.com>
2018-05-23 16:19:42 +0200

    added comments to problem 9

--- problems/9-Special Pythagorean triplet/index.js
+++ problems/9-Special Pythagorean triplet/index.js
@@ -1,5 +1,7 @@
 module.exports = function productOfSpecialPythagoreanTripletWithSumN(n) {
+    // a can't be greater than (n - 3) / 3 because then the rule a < b < c wouldn't be fulfilled
     for (let a = 1; a <= (n - 3) / 3; a++) {
+        // same here for b. Since b always have to be smaller than c
         for (let b = a + 1; b < (n - a) / 2; b++) {
             const c = n - a - b;
 
