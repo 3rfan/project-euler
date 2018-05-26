36ff873072fedf2b1bbd2d6aa6ad1398332a1fce
3rfan <erfan.ebrahimnia@gmail.com>
2018-05-26 22:55:35 +0200

    refactored code of solution 11

--- problems/11-Largest product in a grid/index.js
+++ problems/11-Largest product in a grid/index.js
@@ -4,78 +4,52 @@ module.exports = function largestProducOfFourNumbersInNxNGrid(grid) {
 
     for (let row = 0; row < gridSize; row++) {
         for (let col = 0; col < gridSize; col++) {
+            let product = 0;
+
             // east check
             if (col + 4 < gridSize) {
                 const currentCol = grid[row];
-                const product = currentCol[col] * currentCol[col + 1] * currentCol[col + 2] * currentCol[col + 3];
-
-                if (largestProduct < product) {
-                    largestProduct = product;
-                }
+                product = currentCol[col] * currentCol[col + 1] * currentCol[col + 2] * currentCol[col + 3];
             }
 
             // west check
             if (col + 4 < gridSize) {
                 const currentCol = grid[row];
-                const product = currentCol[col] * currentCol[col - 1] * currentCol[col - 2] * currentCol[col - 3];
-
-                if (largestProduct < product) {
-                    largestProduct = product;
-                }
+                product = currentCol[col] * currentCol[col - 1] * currentCol[col - 2] * currentCol[col - 3];
             }
 
             // south check
             if (row + 4 < gridSize) {
-                const product = grid[row][col] * grid[row + 1][col] * grid[row + 2][col] * grid[row + 3][col]
-
-                if (largestProduct < product) {
-                    largestProduct = product;
-                }
+                product = grid[row][col] * grid[row + 1][col] * grid[row + 2][col] * grid[row + 3][col];
             }
 
             // north check
             if (row - 4 >= 0) {
-                const product = grid[row][col] * grid[row - 1][col] * grid[row - 2][col] * grid[row - 3][col]
-
-                if (largestProduct < product) {
-                    largestProduct = product;
-                }
+                product = grid[row][col] * grid[row - 1][col] * grid[row - 2][col] * grid[row - 3][col];
             }
 
             // south-east check
             if (row + 4 < gridSize && col + 4 < gridSize) {
-                const product = grid[row][col] * grid[row + 1][col + 1] * grid[row + 2][col + 2] * grid[row + 3][col + 3]
-
-                if (largestProduct < product) {
-                    largestProduct = product;
-                }
+                product = grid[row][col] * grid[row + 1][col + 1] * grid[row + 2][col + 2] * grid[row + 3][col + 3];
             }
 
             // south-west check
             if (row + 4 < gridSize && col - 4 >= 0) {
-                const product = grid[row][col] * grid[row + 1][col - 1] * grid[row + 2][col - 2] * grid[row + 3][col - 3]
-
-                if (largestProduct < product) {
-                    largestProduct = product;
-                }
+                product = grid[row][col] * grid[row + 1][col - 1] * grid[row + 2][col - 2] * grid[row + 3][col - 3];
             }
 
             // north-west check
             if (row - 4 >= 0 && col - 4 >= 0) {
-                const product = grid[row][col] * grid[row - 1][col - 1] * grid[row - 2][col - 2] * grid[row - 3][col - 3]
-
-                if (largestProduct < product) {
-                    largestProduct = product;
-                }
+                product = grid[row][col] * grid[row - 1][col - 1] * grid[row - 2][col - 2] * grid[row - 3][col - 3];
             }
 
             // north-east check
             if (row - 4 >= 0 && col + 4 < gridSize) {
-                const product = grid[row][col] * grid[row - 1][col + 1] * grid[row - 2][col + 2] * grid[row - 3][col + 3]
+                product = grid[row][col] * grid[row - 1][col + 1] * grid[row - 2][col + 2] * grid[row - 3][col + 3];
+            }
 
-                if (largestProduct < product) {
-                    largestProduct = product;
-                }
+            if (largestProduct < product) {
+                largestProduct = product;
             }
         }
     }
