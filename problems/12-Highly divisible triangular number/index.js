a6a95ba3250544072800be41358035f43ae839c1
3rfan <erfan.ebrahimnia@gmail.com>
2018-05-26 22:56:15 +0200

    improved algorithm of solution 12

--- problems/12-Highly divisible triangular number/index.js
+++ problems/12-Highly divisible triangular number/index.js
@@ -1,23 +1,34 @@
+function getDivisorsCountOfN(n) {
+    const sqrtOfN = Math.sqrt(n);
+    let divisors = 2;
+
+    for (let j = 2; j <= sqrtOfN; j++) {
+        if (n % j === 0) {
+            divisors += 2;
+        }
+
+        if (j === sqrtOfN) {
+            divisors = divisors - 1;
+        }
+    }
+
+    return divisors;
+}
+
 module.exports = function firstTriangularNumberWithOverNDivisors(n) {
     let triangleNumber = 0;
-    let divisors = 0;
     let i = 1;
+    let divisors = 0;
 
     if (n === 1) return 3;
 
     while (!(divisors > n)) {
-        divisors = 0;
         triangleNumber =  i * (i + 1) / 2;
-        sqrtOfTriangleNumber = Math.sqrt(triangleNumber);
 
-        for (let j = 1; j <= sqrtOfTriangleNumber; j++) {
-            if (triangleNumber % j === 0) {
-                divisors += 2;
-            }
-        }
-
-        if (sqrtOfTriangleNumber * sqrtOfTriangleNumber === triangleNumber) {
-            divisors--;
+        if (i % 2 === 0) {
+            divisors = getDivisorsCountOfN(i / 2) * getDivisorsCountOfN(i + 1);
+        } else {
+            divisors = getDivisorsCountOfN(i) * getDivisorsCountOfN((i + 1) / 2);
         }
 
         i++;
