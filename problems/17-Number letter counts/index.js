0fc05bf107c51d44922c5e28fb07dd7d9a427163
3rfan <erfan.ebrahimnia@gmail.com>
2018-05-31 12:33:49 +0200

    improved solution 17 code

--- problems/17-Number letter counts/index.js
+++ problems/17-Number letter counts/index.js
@@ -1,118 +1,89 @@
-function decomposeNumber(n) {
-    const numberString = n.toString();
-    const length = numberString.length;
-    const decomposedNumbers = [];
-
-    for (let i = 0; i < length; i++) {
-        const restOfNumber = numberString.substr(i, length - i);
-
-        decomposedNumbers.push(restOfNumber);
-    }
-
-    return decomposedNumbers.reverse();
-}
-
-function getLettersSum(numberNames) {
-    let sum = 0;
-
-    for (let i = 0; i < numberNames.length; i++) {
-        sum += numberNames[i].length;
-    }
-
-    return sum;
-}
-
 module.exports = function getNumberLettersCountOfNNumbers(n) {
     if (n > 1000) {
         throw new Error('n must not be larger than 1000');
     }
 
     const writtenOutNumbers = {
-        '1': 'one',
-        '2': 'two',
-        '3': 'three',
-        '4': 'four',
-        '5': 'five',
-        '6': 'six',
-        '7': 'seven',
-        '8': 'eight',
-        '9': 'nine'
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+        20: 'twenty',
+        30: 'thirty',
+        40: 'forty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety'
     };
 
-    const specialNamedNumbers = {
-        '10': 'ten',
-        '11': 'eleven',
-        '12': 'twelve'
-    };
-
-    const specialNamedNumberParts = {
-        '2': 'twen',
-        '3': 'thir',
-        '5': 'fif',
-        '8': 'eigh',
-        '10': 'ten',
-        '11': 'eleven',
-        '12': 'twelve'
-    };
-
-    const allWrittenOutNumbersUntilN = [];
+    let allWrittenOutNumbersSumUntilN = 0;
 
     for (let i = 1; i <= n; i++) {
-        const decomposedNumbers = decomposeNumber(i);
-        const digitsCount = decomposedNumbers.length;
-        const numberParts = [];
+        const digits = i.toString().length;
+        let number = i;
+        let writtenOutNumber = '';
 
-        for (let j = digitsCount - 1; j >= 0; j--) {
-            const decomposedNumberPart = decomposedNumbers[j];
-            const firstDigit = decomposedNumberPart[0];
+        if (digits > 3) {
+            const firstDigit = Math.floor(i / 1000);
+            writtenOutNumber += writtenOutNumbers[firstDigit];
+            writtenOutNumber += 'thousand';
 
-            if (j === 1) {
-                if (firstDigit === '1') {
-                    const secondDigit = decomposedNumberPart[1];
-                    const specialName = specialNamedNumbers[decomposedNumberPart];
-                    const specialNamePart = specialNamedNumberParts[secondDigit];
-                    const specialNumberStart = specialName ? specialName : specialNamePart;
-                    const numberPart = specialNumberStart ? specialNumberStart : writtenOutNumbers[secondDigit];
-
-                    numberParts.push(numberPart);
-
-                    if (numberPart !== specialName) {
-                        numberParts.push('teen');
-                    }
-
-                    j--;
-                } else {
-                    const specialNamePart = specialNamedNumberParts[firstDigit];
-                    const specialNumberStart = specialNamePart ? specialNamePart : writtenOutNumbers[firstDigit];
-                    const numberPart = firstDigit === '4' ? 'for' : specialNumberStart;
-
-                    numberParts.push(numberPart);
-                    
-                    if (numberPart) {
-                        numberParts.push('ty');
-                    }
-                }
-            } else if (j === 2) {
-                numberParts.push(writtenOutNumbers[firstDigit]);
-                numberParts.push('hundred');
-
-                if (decomposedNumbers[j - 1] !== '00') {
-                    numberParts.push('and');
-                }
-            } else if (j === 3) {
-                numberParts.push(writtenOutNumbers[firstDigit]);
-                numberParts.push('thousand');
-
-                if (decomposedNumbers[j - 1] === '000') {
-                    break;
-                }
-            } else {
-                numberParts.push(writtenOutNumbers[firstDigit]);
+            if (i % 1000 === 0) {
+                allWrittenOutNumbersSumUntilN += writtenOutNumber.length;
+                continue;
             }
+
+            number -= firstDigit * 1000;
+        }
+        
+        if (digits > 2) {
+            const firstDigit = Math.floor(number / 100);
+            writtenOutNumber += writtenOutNumbers[firstDigit];
+            writtenOutNumber += 'hundred';
+
+            if (i % 100 === 0) {
+                allWrittenOutNumbersSumUntilN += writtenOutNumber.length;
+                continue;
+            }
+
+            writtenOutNumber += 'and';
+            number -= firstDigit * 100;
         }
 
-        allWrittenOutNumbersUntilN.push(numberParts.join(''));
+        if (digits > 1) {
+            const productOfTen = Math.floor(number / 10) * 10;
+
+            if (writtenOutNumbers[number]) {
+                writtenOutNumber += writtenOutNumbers[number];
+                allWrittenOutNumbersSumUntilN += writtenOutNumber.length;
+                continue;
+            }
+
+            writtenOutNumber += writtenOutNumbers[productOfTen];
+            number -= productOfTen;
+        }
+
+        writtenOutNumber += writtenOutNumbers[number];
+        
+        allWrittenOutNumbersSumUntilN += writtenOutNumber.length;
     }
 
-    return getLettersSum(allWrittenOutNumbersUntilN);
+    return allWrittenOutNumbersSumUntilN;
 };
